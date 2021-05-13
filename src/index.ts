180b2cbd60e8c797cbeb55276a057be4b3c65646
Dominik <dmoreitz@gmail.com>
2021-05-13 11:00:23 +1000

    WIP - main screen

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,20 @@ import * as path from 'path'
 let mainWindow: BrowserWindow | null
 
 function createWindow() {
-    // TODO: place the window in the last position, or centered if not opened before
-    mainWindow = new BrowserWindow({
+  // TODO: place the window in the last position, or centered if not opened before
+  mainWindow = new BrowserWindow({
     width: 1024,
     height: 768,
     minWidth: 800,
     minHeight: 600,
+    title: "Halo",
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: false,
     },
   });
 
   mainWindow.loadURL(`file://${path.join(__dirname, './index.html')}`,);
-  
+
   mainWindow.on('closed', () => {
     mainWindow = null
   });
